0680adc669ae65514f74ffdb93bb80d40d6ef624
Naif <hello@builtbynaif.com>
2019-06-30 11:13:32 +0530

    ms24

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,11 +4,13 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 import Responsive from './modules/Responsive';
+import HeroSlider from './modules/HeroSlider';
 import Modal from './modules/Modal';
 import SlideOnScroll from './modules/SlideOnScroll';
 import SlideOnScrollLeft from './modules/SlideOnScrollLeft';
 
 var mobileMenu = new MobileMenu();
+var heroSlider = new HeroSlider(); 
 var responsive = new Responsive(); 
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
